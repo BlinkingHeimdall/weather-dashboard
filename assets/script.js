365cf0cb1eb5ec7c1070fc359441cfa43b07f45c
Jason Adams <jmadams41@gmail.com>
2022-05-15 18:49:00 -0600

    added event listeners

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+document.getElementById("search").addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("click click");
+})
+
+
+
+
+
+
+// event listener for all of the city buttons after they are created 
+btns = document.getElementsByClassName("city-btn");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        console.log("i was clicked");
+    })
+}
