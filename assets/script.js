6e8460e39d8e224cd3fe3fe938af3b67cc7ea2d8
Jason Adams <jmadams41@gmail.com>
2022-05-19 17:01:01 -0600

    made some changes to day 1 through 5

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 let cityInput = document.querySelector('.input');
 let submitBtn = document.querySelector('#search');
+let currentCityEl = document.querySelector("current-city")
 
 let submitBtnHandler = function(event) {
     event.preventDefault();
@@ -7,8 +8,9 @@ let submitBtnHandler = function(event) {
     let currentCity = cityInputEl.value.trim();
 
     if (currentCity) {
-        getCity(currentCity);
+        getWeather(currentCity);
         cityInputEl.value = "";
+        currentCityEl.innerText = currentCity;
     }
     else
     {
@@ -16,12 +18,11 @@ let submitBtnHandler = function(event) {
     }
 }
 
-let getCity = function(currentCity) {
+let getWeather = function(currentCity) {
     let apiURL = "https://api.openweathermap.org/data/2.5/weather?q=" + currentCity + "&units=imperialappid=15022751718f984d0ffd39a5ca1c8332";
     fetch(apiURL).then(function(response) {
         response.json().then(function(data) {
-            console.log(data.coord.lat);
-            console.log(data.coord.lon);
+
             let lat = data.coord.lat;
             let lon = data.coord.lon;
 
@@ -29,14 +30,48 @@ let getCity = function(currentCity) {
 
             fetch(apiURL2).then(function(response) {
                 response.json().then(function(data) {
-                    console.log(data);
-                console.log(data.current.temp);
-                console.log(data.current.wind_speed);
-                console.log(data.current.humidity);
-                console.log(data.current.uvi);
+                    // weather icon
+                var icon= data.current.weather[0].icon;
+                var iconLink = "https://openweathermap.org/img/w/" + icon + ".png";
+                console.log(iconLink);
+
+
+
+
+
+                // current temp added to html
+                var currentTempEl = document.getElementById("temp");
+                currentTempEl.textContent = "Temperature: " + data.current.temp + " Degrees";
+
+                // wind speed
+                var currentWindEl = document.getElementById("wind");
+                currentWindEl.textContent = "Wind: " + data.current.wind_speed + " MPH"
+
+                //humidity
+                var currentHumidityEl = document.getElementById("humidity");
+                currentHumidityEl.textContent = "Humidity: " + data.current.humidity + " %"
+
+                // uv index
+                var uviEl = document.getElementById("uvi");
+                uviEl.textContent = "UV Index: " + data.current.uvi 
+
+                console.log(data.daily[0])
+
+                var tempEl = document.getElementById("day1temp");
+                tempEl.textContent = "Temp: " + data.daily[0].temp.day + " deg F"
+
+                var windEl = document.getElementById("day1wind");
+                windEl.textContent = "Wind: " + data.daily[0].wind_speed + " mph";
+
+                var humidEl = document.getElementById("day1humid");
+                humidEl.textContent = "Humidity: " + data.daily[0].humidity + " %"
                 })
             })
         });
+        let displayWeather = function (getWeather) {
+
+            
+        }
     });
 };
 
